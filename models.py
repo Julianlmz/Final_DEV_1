5978ea8a651bdb8bfe947b40d6245c7c193e3f26
Julianlmz <jsleal77@ucatolica.edu.co>
2025-12-05 08:32:30 -0500

    Creacion model JugadorBase y Jugador con id

--- models.py
+++ models.py
@@ -1,6 +1,12 @@
-class Jugador():
-    pass
+from sqlmodel import SQLModel, Field, Relationship
 
+class JugadorBase(SQLModel):
+    name: str | None = Field(description="Nombre Jugador")
+    numero: int | None = Field(description="")
+    status: bool | None = Field(description="User status", default=True)
+
+class Jugador(JugadorBase, table=True):
+    id: int | None = Field(default=None, primary_key=True)
 
 class Estadistica():
     pass

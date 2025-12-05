76512e142d04433982fe1e0beb1e5c72ba45b855
Julianlmz <jsleal77@ucatolica.edu.co>
2025-12-05 09:44:08 -0500

    Cambios para incluir router en main

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 from fastapi import FastAPI
+#from database import create_tables
+import jugador
 
 app = FastAPI(title="Sigmotoa FC")
 
+#create_tables()
+
+app.include_router(jugador.router)
 
 @app.get("/")
 async def root():

4e424d2930b0d78200a402f773ecf11688bc948a
Julianlmz <jsleal77@ucatolica.edu.co>
2025-12-05 08:51:43 -0500

    Enumeraciones de Estado, Pie Dominante y Posicion, Jugador base con datos mas extendidos

--- models.py
+++ models.py
@@ -1,9 +1,29 @@
 from sqlmodel import SQLModel, Field, Relationship
+from enum import Enum
+
+class Estado(str,Enum):
+    Activo = "Activo"
+    Inactivo = "Inactivo"
+
+class PieDominante(str,Enum):
+    Zurdo = "Zurdo"
+    Diestro = "Diestro"
+
+class Posicion(str,Enum):
+    DefensaLateral = "Defensa Lateral"
+    DefensaCentral = "Defensa Central"
+    VolanteDefensivo = "Volante Defensivo"
+    Extremo = "Extremo"
+    Delantero = "Delantero"
 
 class JugadorBase(SQLModel):
     name: str | None = Field(description="Nombre Jugador")
-    numero: int | None = Field(description="")
-    status: bool | None = Field(description="User status", default=True)
+    numero: int | None = Field(description="Numero de Camiseta")
+    nacionalidad: str | None = Field(description="Nacionalidad del Jugador")
+    estatura: int | None = Field(description="Estatura del Jugador")
+    peso: int | None = Field(description="Peso del Jugador en Kg")
+    pie_dominante: PieDominante
+    estado: Estado
 
 class Jugador(JugadorBase, table=True):
     id: int | None = Field(default=None, primary_key=True)
